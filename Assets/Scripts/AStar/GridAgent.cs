d1cc55aaa9f42e10d5c3062304514ae652df7b11
Gunn3r1995 <smithateden@hotmail.com>
2018-02-01 16:08:11 +0000

    Seperating GridAgent and Guard scripts

---
+++ Assets/Scripts/AStar/GridAgent.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+namespace Assets.Scripts.AStar
+{
+    public class GridAgent : MonoBehaviour
+    {
+        private Vector3[] _path;
+        private int _targetIndex;
+        private float _speed;
+        private ThirdPersonCharacter _character;
+
+
+        private void Awake()
+        {
+            _character = GetComponent<ThirdPersonCharacter>();
+        }
+
+        public float GetSpeed() {
+            return _speed;
+        }
+
+        public void SetSpeed(float speed) {
+            this._speed = speed;
+        }
+
+        public void SetDestination(Vector3 currentPosition, Vector3 targetPosition) {
+            PathRequestManager.RequestPath(currentPosition, targetPosition, this.OnPathFound);
+        }
+
+        public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
+        {
+            if (!pathSuccessful) return;
+
+            _path = newPath;
+            StopCoroutine("FollowPath");
+            StartCoroutine("FollowPath");
+        }
+
+        public void StopMoving(bool crouch, bool jump){
+            _character.Move(Vector3.zero, crouch, jump);
+        }
+
+        private IEnumerator FollowPath()
+        {
+            var currentWaypoint = _path[0];
+
+            while (true)
+            {
+                if (transform.position == currentWaypoint)
+                {
+                    _targetIndex++;
+                    if (_targetIndex >= _path.Length)
+                    {
+                        yield break;
+                    }
+                    currentWaypoint = _path[_targetIndex];
+                }
+
+                transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, _speed * Time.deltaTime);
+                _character.Move((currentWaypoint - transform.position).normalized * _speed, false, false);
+                yield return null;
+            }
+        }
+
+        public void OnDrawGizmos()
+        {
+            if (_path == null) return;
+
+            for (var i = _targetIndex; i < _path.Length; i++)
+            {
+                Gizmos.color = Color.black;
+                Gizmos.DrawCube(_path[i], Vector3.one);
+
+                Gizmos.DrawLine(i == _targetIndex ? transform.position : _path[i - 1], _path[i]);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Guard.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using Assets.Scripts.AStar;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Guard : MonoBehaviour
+    {
+        private GridAgent _gridAgent;
+        public enum State { Patrol, Alert, Search, Chase }
+        public State state;
+
+		// Patrolling
+		public GameObject[] Waypoints;
+		public bool RandomWaypoints;
+		public float PatrolSpeed = 1.0f;
+		private int _waypointIndex = 0;
+
+		private bool _patrolling;
+
+        void Start()
+        {
+			state = State.Patrol;
+            _gridAgent = GetComponent<GridAgent>();
+        }
+
+        void Update()
+        {
+            FSM();
+        }
+
+		private void FSM()
+		{
+			switch (state)
+			{
+				case State.Patrol:
+					if (!_patrolling)
+					{
+                        StartCoroutine(Patrol());
+					}
+					break;
+				case State.Alert:
+					break;
+				case State.Search:
+					print("Searching");
+					break;
+				case State.Chase:
+					print("Chase State");
+					break;
+			}
+		}
+
+		private IEnumerator Patrol()
+		{
+			print("Patrolling");
+			_patrolling = true;
+
+			while (state == State.Patrol)
+			{
+                _gridAgent.SetSpeed(PatrolSpeed);
+				var distance = Vector3.Distance(transform.position, Waypoints[_waypointIndex].transform.position);
+				if (distance >= 2.0f)
+				{
+                    _gridAgent.SetDestination(transform.position, Waypoints[_waypointIndex].transform.position);
+				}
+				if (distance <= 2.0f)
+				{
+                    if (RandomWaypoints)
+                    {
+                        _waypointIndex = Random.Range(0, Waypoints.Length);
+                    }
+                    else
+                    {
+                        _waypointIndex += 1;
+                        if (_waypointIndex >= Waypoints.Length)
+                        {
+                            _waypointIndex = 0;
+                        }
+                    }
+                    _gridAgent.SetDestination(transform.position, Waypoints[_waypointIndex].transform.position);
+				}
+				else
+				{
+                    _gridAgent.StopMoving(false, false);
+				}
+
+				yield return null;
+			}
+
+			_patrolling = false;
+		}
+
+		public void OnDrawGizmos()
+		{
+            if (Waypoints.Length >= 1)
+            {
+                var startPosition = Waypoints[0].transform.position;
+                var previousPosition = startPosition;
+
+                foreach (var waypoint in Waypoints)
+                {
+                    Gizmos.DrawSphere(waypoint.transform.position, 0.3f);
+                    Gizmos.DrawLine(previousPosition, waypoint.transform.position);
+                    previousPosition = waypoint.transform.position;
+                }
+
+                Gizmos.DrawLine(previousPosition, startPosition);
+            }
+		}
+    }
+}
