412038e7f1b10f50bf5079490808d7fedc735638
Gunn3r1995 <smithateden@hotmail.com>
2018-02-23 12:52:07 +0000

    travis

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -37,16 +37,3 @@ echo "Attempting to build $project for Linux"
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
-
-if defined?(RUBY_ENGINE) && RUBY_ENGINE == "ruby" && RUBY_VERSION >= "1.9"
-  module Kernel
-    alias :__at_exit :at_exit
-    def at_exit(&block)
-      __at_exit do
-        exit_status = $!.status if $!.is_a?(SystemExit)
-        block.call
-        exit exit_status if exit_status
-      end
-    end
-  end
-end
