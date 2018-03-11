abad8f9ddd11bad5e2c5fbb6e674cdc05e6d9a52
Gunn3r1995 <smithateden@hotmail.com>
2018-03-11 17:27:20 +0000

    level select

---
+++ Assets/Scripts/UIShowImage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIShowImage : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    public GameObject DisplayGameObject;
+
+	// Use this for initialization
+	private void Start () {
+        print("Game Object");
+        DisplayGameObject.SetActive(false);
+	}
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+		print("Mouse Enter");
+		DisplayGameObject.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        print("Mouse Exit");
+		DisplayGameObject.SetActive(false);
+    }
+}
